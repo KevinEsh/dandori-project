19aabb1bcb996ad93531d130cb2d5ef19d2fd907
Kevin Esh <52937054+KevinEsh@users.noreply.github.com>
2025-05-18 00:47:49 +0000

    hyperparameter optimization

--- src/02-experiment-tracking/train.py
+++ src/02-experiment-tracking/train.py
@@ -1,16 +1,21 @@
 import os
+import yaml
 import pickle
 import click
+import mlflow
 import numpy as np
 
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import root_mean_squared_error
 
 
+def load_config(path="config.yaml"):
+    with open(path, "r") as f:
+        return yaml.safe_load(f)
+
 def load_pickle(filename: str) -> tuple[np.ndarray, np.ndarray]:
     with open(filename, "rb") as f_in:
         return pickle.load(f_in)
-
 
 @click.command()
 @click.option(
@@ -18,17 +23,37 @@ def load_pickle(filename: str) -> tuple[np.ndarray, np.ndarray]:
     default="./data/middle",
     help="Location where the processed NYC taxi trip data was saved"
 )
-def run_train(data_path: str):
+@click.option(
+    "--config_file",
+    default="src/02-experiment-tracking/rfr_config.yaml",
+    help="Path to config file. It should containg MLFlow metadata, tags and hyperparameters"
+)
+def run_train(data_path: str, config_file: str):
+    config = load_config(config_file)
+    # mlflow.create_experiment(con)
+    mlflow.set_tracking_uri(config['tracking_uri'])
+    mlflow.set_experiment(config['experiment']['name'])
+    mlflow.sklearn.autolog(extra_tags=config['model']['tags'])
 
-    X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
-    X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
+    with mlflow.start_run() as this_run:
+        # mlflow.set_tags(config['tags'])
+        # mlflow.log_params(config['hyp_params'])
 
-    rf = RandomForestRegressor(max_depth=10, random_state=0) #5, 0
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_val)
+        X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+        X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
 
-    rmse = root_mean_squared_error(y_val, y_pred)
-    print(rmse)
+        rf = RandomForestRegressor(**config['model']['hyp_params'])
+        rf.fit(X_train, y_train)
+
+        metrics = {
+            # 'validation_': root_mean_squared_error(y_train, rf.predict(X_train)),
+            'validation_root_mean_squared_error': root_mean_squared_error(y_val, rf.predict(X_val))
+        }
+
+        mlflow.log_metrics(metrics, synchronous=True)
+    
+    last_run = mlflow.get_run(this_run.info.run_id)
+    print(last_run.data.metrics, last_run.data.params)
 
 
 if __name__ == '__main__':
